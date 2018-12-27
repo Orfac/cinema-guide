b9128a02ae72c3cf173d31f90013a6225e52714b
arseniy <molodeckij.b.2017@fml31.ru>
2018-12-27 22:07:51 +0300

    added todo for rating controller

--- KinoGuideProject/src/main/java/ru/kinoguide/controller/RatingController.java
+++ KinoGuideProject/src/main/java/ru/kinoguide/controller/RatingController.java
@@ -8,12 +8,18 @@ import ru.kinoguide.repository.RatingRepository;
 @RequestMapping("/rating")
 public class RatingController {
 
+    // TODO добавить filmRepository
     @Autowired
     private RatingRepository ratingRepository;
 
+    // TODO добавить User и pageableRequest в findAll
     @RequestMapping("/{userId}")
     public String getUserRating(ModelMap model) {
         model.put("rates", ratingRepository.findAll());
         return "userRating";
     }
+
+    // TODO SetRating для изменения оценки {Post}
+
+
 }
