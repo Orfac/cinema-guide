18dd6057229e1da88bcc6b6583ad14f2ce165289
arseniy <molodeckij.b.2017@fml31.ru>
2018-12-27 22:28:25 +0300

    added todo for other controllers

---
+++ KinoGuideProject/src/main/java/ru/kinoguide/controller/SessionController.java
@@ -0,0 +1,7 @@
+package ru.kinoguide.controller;
+
+public class SessionController {
+    // TODO добавить метод получения информации по сеансу {GET}
+    // TODO добавить метод изменения статуса заявки {POST}
+    // TODO добавить метод получения сеансов по кинотеатру {GET}
+}
