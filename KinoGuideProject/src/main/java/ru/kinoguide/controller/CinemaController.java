e04a1bd190740123068e5cc2e29d96c61d9e5760
arseniy <molodeckij.b.2017@fml31.ru>
2019-01-25 20:03:29 +0300

    updated Controller to return page by id added 2 repos for cinema network and cinema theatres added register page and cinemaTheatre page

--- KinoGuideProject/src/main/java/ru/kinoguide/controller/CinemaController.java
+++ KinoGuideProject/src/main/java/ru/kinoguide/controller/CinemaController.java
@@ -1,10 +1,30 @@
 package ru.kinoguide.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.kinoguide.entity.CinemaNetwork;
+import ru.kinoguide.repository.CinemaNetworkRepository;
+import ru.kinoguide.repository.CinemaTheatreRepository;
 
 @Controller
-@RequestMapping("/cinema-hall")
+@RequestMapping("/cinema")
 public class CinemaController {
-    // TODO возвращает информацию о кинотеатре {GET}
+
+    @Autowired
+    private CinemaTheatreRepository cinemaTheatreRepository;
+
+    @Autowired
+    private CinemaNetworkRepository cinemaNetworkRepository;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    String getId(ModelMap model, @RequestParam String cinemaId){
+        int intId = Integer.parseInt(cinemaId);
+        model.put("cinemaTheatre", cinemaTheatreRepository.findById(intId));
+        return "cinemaTheatre";
+    }
 }
