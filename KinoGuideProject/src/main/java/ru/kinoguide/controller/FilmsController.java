2443f9b147eb8a8ce3abf8d92a7486b1cb0dbe08
arseniy <molodeckij.b.2017@fml31.ru>
2019-01-26 20:51:27 +0300

    Added film repo and film page

--- KinoGuideProject/src/main/java/ru/kinoguide/controller/FilmsController.java
+++ KinoGuideProject/src/main/java/ru/kinoguide/controller/FilmsController.java
@@ -1,10 +1,17 @@
 package ru.kinoguide.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.kinoguide.entity.Film;
 import ru.kinoguide.repository.FilmRepository;
+
+import java.util.Date;
 
 @Controller
 @RequestMapping("/film")
@@ -17,6 +24,14 @@ public class FilmsController {
     public String index(ModelMap model) {
         model.put("films", filmRepository.findAll());
         return "billboard";
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public String index(ModelMap model, @PathVariable("id") int id,
+                        @RequestParam(required=false) @DateTimeFormat(pattern="MMddyyyy") Date day){
+        Film film = filmRepository.findById(id);
+
+        return "film";
     }
 
 }
