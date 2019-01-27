9410de41bdb3638805cdafbbb71f82b044b35edb
GuyByDefault <lexmint@rambler.ru>
2019-01-28 00:44:22 +0300

    Rating list

--- KinoGuideProject/src/main/java/ru/kinoguide/controller/RatingController.java
+++ KinoGuideProject/src/main/java/ru/kinoguide/controller/RatingController.java
@@ -1,26 +1,44 @@
 package ru.kinoguide.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.kinoguide.entity.User;
 import ru.kinoguide.repository.RatingRepository;
+import ru.kinoguide.repository.UserRepository;
 
 @Controller
 @RequestMapping("/rating")
 public class RatingController {
 
-    // TODO добавить filmRepository
+    // TODO SetRating для изменения оценки {Post}
+    // TODO {придумать название метода} получает массив схожих юзеров и их оценки и их схожесть
+
+
     @Autowired
     private RatingRepository ratingRepository;
 
-    // TODO добавить User и pageableRequest в findAll
-    @RequestMapping("/{userId}")
-    public String getUserRating(ModelMap model) {
-        model.put("rates", ratingRepository.findAll());
+    @Autowired
+    private UserRepository userRepository;
+
+    @RequestMapping("")
+    public String getRateByUserId(
+            ModelMap model,
+            @RequestParam(name = "user") Integer userId,
+            @RequestParam(name = "page", required = false, defaultValue = "1") Integer page,
+            @RequestParam(name = "size", required = false, defaultValue = "20") Integer ordersOnPage
+    ) {
+        User user = userRepository.findOne(userId);
+        if (user == null) {
+            model.addAttribute("error", "Пользователь не найден");
+            return "error";
+        }
+        model.put("rates", ratingRepository.findAllByUser(user, new PageRequest(page, ordersOnPage, Sort.Direction.DESC, "date")));
         return "userRating";
     }
 
-    // TODO SetRating для изменения оценки {Post}
-    // TODO {придумать название метода} получает массив схожих юзеров и их оценки и их схожесть
 }
