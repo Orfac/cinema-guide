076c4347efeaaa13dfd3b834fe9e87b92d00b199
GuyByDefault <lexmint@rambler.ru>
2019-03-09 19:44:41 +0300

    Repo fix

--- KinoGuideProject/src/main/java/ru/kinoguide/controller/OrderController.java
+++ KinoGuideProject/src/main/java/ru/kinoguide/controller/OrderController.java
@@ -45,8 +45,6 @@ public class OrderController {
         this.relevanceService = relevanceService;
     }
 
-
-
     @RequestMapping("")
     @Secured({"ROLE_USER"})
     public String getLoggedUserOrders(
--- KinoGuideProject/src/main/java/ru/kinoguide/repository/SessionRepository.java
+++ KinoGuideProject/src/main/java/ru/kinoguide/repository/SessionRepository.java
@@ -4,30 +4,30 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import ru.kinoguide.entity.CinemaTheatre;
 import ru.kinoguide.entity.Film;
 import ru.kinoguide.entity.Session;
 
 import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.List;
 
 public interface SessionRepository extends PagingAndSortingRepository<Session, Integer> {
     @Query("select s from Session s where s.startTime > CURRENT_DATE")
     Page<Session> findAllSinceNow(Pageable pageable);
 
-    @Query("select s from Session s where s.startTime > instant")
-    Page<Session> findAllSinceDate(Pageable pageable, Instant instant);
+    @Query("select s from Session s where s.startTime > :instant")
+    Page<Session> findAllSinceDate(Pageable pageable, @Param("instant") Instant instant);
 
-    @Query("select s from Session s where s.film.name in (filmsNames)")
-    List<Session> findSessionsByFilms(String[] filmsNames);
+    @Query("select s from Session s where s.film.name in (:filmsNames)")
+    List<Session> findSessionsByFilms(@Param("filmsNames") String[] filmsNames);
 
     Page<Session> findByFilmAndStartTimeBetweenOrderByStartTimeAsc(Pageable pageable, Film film, Instant start, Instant end);
 
     List<Session> findByFilmAndStartTimeBetweenOrderByStartTimeAsc(Film film, Instant start, Instant end);
 
     List<Session> findByCinemaHallCinemaTheatreAndStartTimeBetweenOrderByStartTimeAsc(CinemaTheatre cinemaTheatre, Instant start, Instant end);
+
     @Query("select s from Session s where s.startTime > CURRENT_DATE")
     List<Session> findAllSinceNow();
 
